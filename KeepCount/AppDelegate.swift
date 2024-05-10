449a631961dfa37c37faf6f6702f4aab9c05184f
nishchaykarle <nishchaykarle@gmail.com>
2024-05-10 16:00:53 -0500

    Initial Commit

---
+++ KeepCount/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  KeepCount
+//
+//  Created by Nishchay Karle on 5/10/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ KeepCount/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeepCount
+//
+//  Created by Nishchay Karle on 5/10/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
